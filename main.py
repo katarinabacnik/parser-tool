b4e4ea1f6324503258730adc4d6289b3871d850e
katarina <katarina.bacnik@nib.si>
2023-06-26 13:51:18 +0200

    za vse file

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-file_one = "data/file_a.txt"
-file_two = "data/file_b.txt"
-final_merged_file = "data/merged_file.txt"
+import os
+
+data_folder_name = "data"
+final_merged_file = "merged_file.txt"
 remove_empty_lines = True
+
+
+def get_absolute_file_paths(folder_name: str) -> list[str]:
+    absolute_paths = []
+    for dirpath, _, filenames in os.walk(folder_name):
+        for filename in filenames:
+            absolute_path = os.path.abspath(os.path.join(dirpath, filename))
+            absolute_paths.append(absolute_path)
+    return absolute_paths
 
 
 def read_text_file_to_list(file_path: str) -> list[str]:
@@ -25,18 +35,16 @@ def write_list_to_text(file_data: list[str], file_path: str) -> None:
 
 
 # Read files
-file_a_data = read_text_file_to_list(file_one)
-file_b_data = read_text_file_to_list(file_two)
+all_files_paths = get_absolute_file_paths(data_folder_name)
+merged_list = []
+for path in all_files_paths:
+    file_data = read_text_file_to_list(path)
+    merged_list += file_data
 
-# Merge files
-merged_list = file_a_data + file_b_data
 
 # Remove new line characters
 merged_list = strip_new_line_characters(merged_list)
 
-# merged_list_no_newlines = []
-# for line in merged_list:
-# merged_list_no_newlines.append(line.strip())
 
 # Remove empty lines
 if remove_empty_lines:
@@ -44,9 +52,3 @@ if remove_empty_lines:
 
 # Write to file
 write_list_to_text(merged_list, final_merged_file)
-
-
-# # preberemo obe datoteki
-# with open("readme.txt") as f:
-#     lines = f.readlines()
-# # združimo datoteki in jih shranimo v novo datoteko
