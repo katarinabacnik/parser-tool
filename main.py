32dd5309c904d02d4d563d62ed78d29edd31986b
katarina <katarina.bacnik@nib.si>
2023-06-26 15:00:32 +0200

    sys

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
+"""
+Tool for merging text files into one file.
+
+Usage:
+python main.py --output_path=results/merged.txt --input_path=data/file* --remove-empty-lines
+
+Options:
+    --input-path=<input_path>
+    --output-path<output_path> Default: merged.txt
+    --remove-empty-lines Default:False
+
+"""
+
+import fnmatch
 import os
+import sys
 
-data_folder_name = "data"
-final_merged_file = "merged_file.txt"
-remove_empty_lines = True
+print(sys.argv)
 
 
-def get_absolute_file_paths(folder_name: str) -> list[str]:
+def pars_arguments_input_path(arguments: list[str]) -> str:
+    """Parses input path from arguments."""
+    pass
+
+
+def pars_arguments_output_path(arguments: list[str]) -> str:
+    """Parses output path from arguments."""
+    pass
+
+
+def pars_arguments_remove_empty_lines(arguments: list[str]) -> str:
+    """Parses from arguments."""
+    pass
+
+
+def get_absolute_file_paths(folder_name: str, filter: str) -> list[str]:
     absolute_paths = []
     for dirpath, _, filenames in os.walk(folder_name):
         for filename in filenames:
-            absolute_path = os.path.abspath(os.path.join(dirpath, filename))
-            absolute_paths.append(absolute_path)
+            if fnmatch.fnmatch(filename, filter):
+                absolute_path = os.path.abspath(os.path.join(dirpath, filename))
+                absolute_paths.append(absolute_path)
     return absolute_paths
 
 
@@ -34,21 +63,32 @@ def write_list_to_text(file_data: list[str], file_path: str) -> None:
             file.write(line + "\n")
 
 
-# Read files
-all_files_paths = get_absolute_file_paths(data_folder_name)
-merged_list = []
-for path in all_files_paths:
-    file_data = read_text_file_to_list(path)
-    merged_list += file_data
+def split_inputh_path(input_path: str) -> tuple[str, str]:
+    """Splits input path into folder path and filter"""
+    # Input: data/reports/repo
 
 
-# Remove new line characters
-merged_list = strip_new_line_characters(merged_list)
+def main():
+    # Parse arguments
+    input_path = pars_arguments_input_path(sys.argy)
+    output_path = pars_arguments_output_path(sys.argv)
+    remove_empty_lines = pars_arguments_remove_empty_lines(sys.argv)
 
+    # Read files
+    all_files_paths = get_absolute_file_paths(data_folder_name, filter_file_name)
+    merged_list = []
+    for path in all_files_paths:
+        file_data = read_text_file_to_list(path)
+        merged_list += file_data
 
-# Remove empty lines
-if remove_empty_lines:
-    merged_list = remove_empty_lines(merged_list)
+    # Remove new line characters
+    merged_list = strip_new_line_characters(merged_list)
 
-# Write to file
-write_list_to_text(merged_list, final_merged_file)
+    # Remove empty lines
+    if remove_empty_lines:
+        merged_list = remove_empty_lines(merged_list)
+
+    # Write to file
+    write_list_to_text(merged_list, final_merged_file)
+
+    print("done!")
