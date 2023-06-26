a59d4a74a33f29024cb4344ad5fc3f8449eea130
katarina <katarina.bacnik@nib.si>
2023-06-26 11:09:27 +0200

    merge

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print("Hello")
+file_one = "data/file_a.txt"
+file_two = "data/file_b.txt"
+final_merged_file = "data/merged_file.txt"
+remove_empty_lines = True
+
+# Read files
+with open(file_one, "r") as file_a:
+    file_a_data = file_a.readlines()
+
+with open(file_two, "r") as file_b:
+    file_b_data = file_b.readlines()
+
+# Merge files
+merged_list = file_a_data + file_b_data
+
+# Remove new line characters
+merged_list = [line.strip() for line in merged_list]
+
+# merged_list_no_newlines = []
+# for line in merged_list:
+# merged_list_no_newlines.append(line.strip())
+
+# Remove empty lines
+if remove_empty_lines:
+    merged_list = [line for line in merged_list if line != ""]
+
+with open(final_merged_file, "w") as merged_file:
+    for line in merged_list:
+        merged_file.write(line + "\n")
+
+
+# # preberemo obe datoteki
+# with open("readme.txt") as f:
+#     lines = f.readlines()
+# # združimo datoteki in jih shranimo v novo datoteko
